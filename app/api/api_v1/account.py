22f56a470e7d97d37fd1d9b089febdece99cf4fd
morteza mollaie <mm.gov.1381@gmail.com>
2025-07-15 22:09:56 +0330

    feat: imeplement verify otp code

--- app/api/api_v1/account.py
+++ app/api/api_v1/account.py
@@ -1,27 +1,15 @@
 from fastapi import Depends, APIRouter, status, HTTPException
 from sqlalchemy.orm import Session
 from app.api import deps
-from app.schemas.account import SignUp, SignUpResponse, SendOtp
+from app.schemas.account import SignUp, SignUpResponse, SendOtp, VerifyOtp
 from app.models.user import User
 from app.models.account import OtpCode
 from app.services.hash_password import Hash
 from datetime import datetime, timedelta
+from app.services.generate_token import create_access_token
 import random
 
 router = APIRouter()
-
-@router.post("/signup", response_model=SignUpResponse, status_code=status.HTTP_201_CREATED)
-def signup(request: SignUp, db: Session = Depends(deps.get_db)):
-    existing_user = db.query(User).filter(User.email == request.email).first()
-    if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="a user with this email already exists.")
-    hash_password = Hash.bcrypt(request.password)
-    user = User(email=request.email, first_name=request.first_name, last_name=request.last_name, password=hash_password)
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
-
 
 @router.post("/send-otp", status_code=status.HTTP_200_OK, tags=["Accounts"])
 def sendOtp(request: SendOtp, db: Session = Depends(deps.get_db)):
@@ -41,4 +29,25 @@ def sendOtp(request: SendOtp, db: Session = Depends(deps.get_db)):
     db.add(new_otp)
     db.commit()
     db.refresh(new_otp)
-    return {"data": "The OTP code was sent successfully."}        
+    return {"data": "The OTP code was sent successfully."}
+
+
+@router.post("/verify-otp", status_code=200, tags=["Accounts"])
+def verify_otp(request: VerifyOtp, db: Session = Depends(deps.get_db)):
+    otp_record = db.query(OtpCode).filter(OtpCode.phone_number == request.phone_number).first()
+
+    if not otp_record:
+        raise HTTPException(status_code=404, detail="OTP code not found.")
+    
+    if otp_record.code != request.code:
+        raise HTTPException(status_code=400, detail="Incorrect OTP code.")
+    
+    time_diff = datetime.utcnow() - otp_record.created_at
+    if time_diff > timedelta(minutes=2):
+        raise HTTPException(status_code=400, detail="OTP code has expired.")
+
+    db.delete(otp_record)
+    db.commit()
+
+    access_token = create_access_token(data={"sub": request.phone_number})
+    return {"access_token": access_token, "token_type": "bearer"}        
